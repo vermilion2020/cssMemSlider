3935eb81348a6340632da185f2fa85ecd6de528d
Mila <99rest9900@gmail.com>
2022-05-14 15:24:46 +0300

    feat: add clider functionality for desktop (Sat, May 14, 2022 03:24:46 PM)

--- cssMemSlider/index.js
+++ cssMemSlider/index.js
@@ -3,7 +3,7 @@ const SLIDER_INFO = [{
   text: 'Наташа, мы все уронили'
 },
 {
-  image: 'chipseci.png',
+  image: 'chipseki.png',
   text: 'Чипсеки!'
 },
 {
@@ -15,15 +15,75 @@ const SLIDER_INFO = [{
   text: 'Толстая собачка'
 }];
 
-function showImage(id, pictures) {
-  const IMG_CONTAINER = document.querySelector('#slider-picture');
-  const NEW_IMAGE = document.createElement('img');
-  NEW_IMAGE.dataset.id = id;
-  NEW_IMAGE.setAttribute('src', `images/${pictures[id].image}`);
-  NEW_IMAGE.setAttribute('alt', pictures[id].text);
-  IMG_CONTAINER.appendChild(NEW_IMAGE);
+class Slider {
+  activeElement = 0;
+
+  constructor(pictures) {
+    this.pictures = pictures;
+  }
+
+  showImage(id) {
+    this.activeElement = id;
+    const IMG_CONTAINER = document.querySelector('#slider-picture');
+    const NEW_IMAGE = document.createElement('img');
+    NEW_IMAGE.dataset.id = id;
+    NEW_IMAGE.setAttribute('src', `images/${this.pictures[id].image}`);
+    NEW_IMAGE.setAttribute('alt', this.pictures[id].text);
+    IMG_CONTAINER.innerHTML = '';
+    IMG_CONTAINER.appendChild(NEW_IMAGE);
+    document.querySelector("#slider-text").innerText = this.pictures[id].text;
+    document.querySelector(".active").classList.remove('active');
+    document.querySelector(`#slider-controls > [data-id="${id}"]`).classList.add('active');
+  }
+
+  sliderControls() {
+    for (let i = 0; i < this.pictures.length; i++) {
+      const NEW_CONTROL = document.createElement('div');
+      i === 0 ? NEW_CONTROL.classList.add('active') : '';
+      NEW_CONTROL.dataset.id = i;
+      this.activeElement = 0;
+      document.querySelector('#slider-controls').appendChild(NEW_CONTROL);
+    }
+  }
 }
 
+function fadeOut(id) {
+  const SLIDER_IMG = document.querySelector('#slider-picture img');
+  const SLIDER_TEXT = document.querySelector('#slider-text');
+  SLIDER_IMG.setAttribute('data-id', id);
+  SLIDER_IMG.classList.add('transition-fade-out');
+  SLIDER_TEXT.classList.add('transition-fade-out');
+};
+
+const SLIDER = new Slider(SLIDER_INFO);
+
 window.addEventListener('load', () => {
-showImage(0, SLIDER_INFO);
+  SLIDER.sliderControls();
+  SLIDER.showImage(0);
 }, true);   
+
+document.querySelector('#slider-controls').addEventListener('click', (event) => {
+  if (event.target.dataset.id !== undefined) {
+    fadeOut(event.target.dataset.id);
+  }});
+
+document.querySelector('#slider-picture').addEventListener('animationend', (animation) => {
+  if (event.target.dataset.id !== undefined) {
+    const SLIDER_IMG = document.querySelector('#slider-picture img');
+    const SLIDER_TEXT = document.querySelector('#slider-text');
+    if (animation.animationName === 'fade-out') {
+      if (animation.target.dataset.id !== undefined) {
+        SLIDER.showImage(animation.target.dataset.id);
+        const SLIDER_IMG = document.querySelector('#slider-picture img');
+        const SLIDER_TEXT = document.querySelector('#slider-text');
+        SLIDER_IMG.classList.remove('transition-fade-out');
+        SLIDER_IMG.classList.add('transition-fade-in');
+        SLIDER_TEXT.classList.remove('transition-fade-out');
+        SLIDER_TEXT.classList.add('transition-fade-in');
+      }
+    } else {
+      SLIDER_IMG.classList.remove('transition-fade-in');
+      SLIDER_TEXT.classList.remove('transition-fade-in');
+    }
+  }
+});
